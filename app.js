87100979b78c708c37fa9f8cf862808dea91dff1
Chuck Langford <field.email@gmail.com>
2015-12-03 11:39:17 -0500

    Made the parent process a bit more resilient

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var cluster = require('cluster');
+var temp = 0;
 
 if (cluster.isMaster) {
   console.log('FORKING...');
-  cluster.fork();
+  var w = cluster.fork();
 
   cluster.on('exit', function(worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died');
@@ -15,9 +16,20 @@ if (cluster.isMaster) {
 
   processes.newProcess.send({another: 'test'});
 
+
   process.on('exit', function() {
     processes.killProcesses();
   });
+
+  process.on('uncaughtException', function(err) {
+    console.log(err);
+    processes.killProcesses();
+    process.exit(1);
+  });
+
+  setInterval(function() {
+    throw new Error('oH NoeS!');
+  }, 15000);
 
   // Call the first function of notchild
   notchild.test();
